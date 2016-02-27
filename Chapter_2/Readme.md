4d1043954d06b67180c532c2f87dc3bf5e289f23
Michael Haney <michael.haney@colorado.edu>
2016-02-27 10:49:29 -0700

    Added answers to Chapter_2
    [#113796049]

---
+++ Chapter_2/Readme.md
@@ -0,0 +1,10 @@
+#Chapter 2: Learn Ruby the Hard Way
+
+>Can you find a key combo in RubyMine to comment out a line of code? 
+
+On https://www.jetbrains.com/ruby/help/keyboard-shortcuts-you-cannot-miss.html, I found a shortcut for #. Simply, you can press `command /` to comment out a whole line.
+
+>Did you get any errors from Rubocop?
+
+I did. Rubocop complained about these use of double quotes where single quotes should be applied.
+I made the necessary adjsutments and satisfied Rubocop.
---
+++ Chapter_2/ex2.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by ruby.
+
+puts "I could have code like this." # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# puts "This won't run."
+
+puts "This will run."
+
+#Find out if you were right about what the # character does and make sure you know what it's called (octothorpe or pound character).
+puts "The # character does in fact make a line a comment with the exception of certain 'hack' code that runs after the # character"
+
+#Take your ex2.rb file and review each line going backward. Start at the last line, and check each word in reverse against what you should have typed
+
+#Did you find more mistakes? Fix them.
+
